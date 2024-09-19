9aec4d812acbcb9f8e8e52a561cd8129dc2cf2f6
VuiDev <nguyenminhdao002@gmail.com>
2024-09-19 21:54:19 +0700

    first commit for start project

---
+++ src/main/java/vn/toilamdev/bookmarket/BookMarketJavaJspSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package vn.toilamdev.bookmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMarketJavaJspSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMarketJavaJspSpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/toilamdev/bookmarket/BookMarketJavaJspSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.toilamdev.bookmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMarketJavaJspSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
