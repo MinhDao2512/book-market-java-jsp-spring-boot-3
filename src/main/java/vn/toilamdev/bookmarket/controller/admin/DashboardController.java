c33b0f6a7fc30980fc438d7db4c6982e5719fc3e
VuiDev <nguyenminhdao002@gmail.com>
2024-09-21 21:51:05 +0700

    thiet ke giao dien admin va chia layout cho trang admin

---
+++ src/main/java/vn/toilamdev/bookmarket/controller/admin/DashboardController.java
@@ -0,0 +1,15 @@
+package vn.toilamdev.bookmarket.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class DashboardController {
+    @GetMapping
+    public String getDashboardPage(Model model) {
+        return "admin/dashboard/show";
+    }
+}
--- src/main/webapp/WEB-INF/view/client/home/show.jsp
+++ src/main/webapp/WEB-INF/view/client/home/show.jsp
@@ -4,7 +4,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Fruitables - Vegetable Website Template</title>
+    <title>StorySwap.vn | Kho tàng sách hay</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
